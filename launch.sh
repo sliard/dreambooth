1e49335409303b793c844cc0f192790558413368
Samuel Liard <samuel.liard@babbar.tech>
2022-12-22 16:22:58 +0000

    first commit

---
+++ launch.sh
@@ -0,0 +1,28 @@
+export MODEL_NAME="runwayml/stable-diffusion-v1-5"
+export OUTPUT_DIR="output"
+
+accelerate launch train_dreambooth.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_name_or_path="stabilityai/sd-vae-ft-mse" \
+  --output_dir=$OUTPUT_DIR \
+  --revision="fp16" \
+  --with_prior_preservation --prior_loss_weight=1.0 \
+  --seed=3434554 \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --train_text_encoder \
+  --mixed_precision="fp16" \
+  --use_8bit_adam \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=1e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --num_class_images=50 \
+  --sample_batch_size=4 \
+  --max_train_steps=800 \
+  --save_interval=400 \
+  --save_sample_prompt="photo of zwx person" \
+  --instance_prompt="photo of zwx person"
+  --class_prompt="photo of a person"
+  --instance_data_dir="./gpeyronnet"
+  --class_data_dir="./person"
